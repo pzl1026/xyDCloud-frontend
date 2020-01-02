b2a9955a2c0fce8a9266fb127fdf53a59b87c5df
pzl1026 <pzl1026@aliyun.com>
2020-01-02 21:04:24 +0800

    项目暂停，启动下载

--- app/src/pages/cloud/index.js
+++ app/src/pages/cloud/index.js
@@ -2,10 +2,10 @@ import React, {PureComponent, Fragment} from 'react';
 import {connect} from 'dva';
 import withRouter from 'umi/withRouter';
 import {routerRedux} from 'dva/router';
-import {Row, Col, Dropdown, Menu, Icon} from 'antd';
-import IconBlock from '@components/IconBlock';
 import DownloadHeader from '@components/DownloadHeader';
+import DownlistLi from './components/DownlistLi';
 import './index.scss';
+const { ipcRenderer } = window.require('electron');
 
 function mapStateToProps(state) {
     return {
@@ -16,78 +16,6 @@ function mapStateToProps(state) {
 
 const header = ['项目名称', '本地目录', '任务状态', '操作'];
 
-const menu = (
-    <Menu>
-        <Menu.Item key="0">
-            <div className="dl-li-action-menu">
-                <img src="" alt=""/>
-                <span>打开本地目录</span>
-            </div>
-        </Menu.Item>
-        <Menu.Item key="1">
-            <div className="dl-li-action-menu">
-                <img src="" alt=""/>
-                <span>打开云端网页</span>
-            </div>
-        </Menu.Item>
-        <Menu.Item key="2">
-            <div className="dl-li-action-menu">
-                <img src="" alt=""/>
-                <span>编辑下载任务</span>
-            </div>
-        </Menu.Item>
-        <Menu.Item key="3">
-            <div className="dl-li-action-menu">
-                <img src="" alt=""/>
-                <span>删除下载任务</span>
-            </div>
-        </Menu.Item>
-    </Menu>
-);
-
-function DownlistLi(props) {
-    return (
-        <Fragment>
-            {props
-                .projects
-                .map(item => {
-                    return (
-                        <Row className="dl-li" key={item.id}>
-                            <Col span={6}>
-                                <span>{item.name}</span>
-                            </Col>
-                            <Col span={6}>
-                                <span>{item.localPath || '未设置'}</span>
-                            </Col>
-                            <Col span={6}>
-                                <span>{item.isPause ? '暂停中':'下载中'}</span>
-                            </Col>
-                            <Col span={6}>
-                                <div className="dl-action">
-                                    {/* <img src="" alt=""/> */}
-                                    {/* <IconBlock iconName="fm-share.svg" direction="left"></IconBlock> */}
-                                    <span className="cloud-li-play">
-                                        {item.isPause ? 
-                                        <IconBlock iconName="play.svg"></IconBlock> : 
-                                        <IconBlock iconName="pause.svg"></IconBlock>}
-                                    </span>
-                                    <Dropdown overlay={menu} trigger={['click']} placement="bottomCenter">
-                                        <div className="dl-action-btn">
-                                            <span>
-                                                <IconBlock iconName="more.svg"></IconBlock>
-                                                {/* <Icon type="ellipsis"/> */}
-                                            </span>
-                                        </div>
-                                    </Dropdown>
-                                </div>
-                            </Col>
-                        </Row>
-                    )
-                })
-}
-        </Fragment>
-    );
-}
 
 function DownlistEmpty(props) {
     return (
@@ -103,6 +31,17 @@ function DownlistEmpty(props) {
 class CloudContainer extends PureComponent {
     componentDidMount() {
         console.log(this.props, ' 9999')
+        ipcRenderer.on('changed-pause-status', (event, projects) => {
+            console.log(projects, 'userStore.projects userStore.projects ')
+            this.props.dispatch({
+                type: 'project/saveProjects',
+                payload: projects
+            });
+        });
+    }
+
+    changePause = (projectId) => {
+        ipcRenderer.send('change-pause-status', projectId);
     }
 
     toCloudCreate = () => {
@@ -121,7 +60,7 @@ class CloudContainer extends PureComponent {
                 <div className="download-list">
                     <DownloadHeader header={header}></DownloadHeader>
                     {projects.length > 0 ? 
-                    <DownlistLi {...this.props}></DownlistLi> :
+                    <DownlistLi {...this.props} changePause={this.changePause}></DownlistLi> :
                     <DownlistEmpty></DownlistEmpty>}
                 </div>
             </Fragment>
