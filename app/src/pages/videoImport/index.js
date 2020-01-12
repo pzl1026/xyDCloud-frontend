ef7d239c11804c2ac3b84b9b8283669ca0c3ffb0
pzl1026 <pzl1026@aliyun.com>
2020-01-12 10:54:57 +0800

    commit

--- app/src/pages/videoImport/index.js
+++ app/src/pages/videoImport/index.js
@@ -5,6 +5,7 @@ import {Row, Col, Checkbox, Menu, Dropdown, Icon} from 'antd';
 import PageHeader from '@components/PageHeader';
 import {routerRedux} from 'dva/router';
 import './index.scss';
+const { ipcRenderer } = window.require('electron');
 
 const menu = (
     <Menu>
@@ -48,7 +49,16 @@ function VideoLi (props) {
                 <div className="video-li-info">
                     <span>{item.name}</span>
                     <span>{size}M</span>
-                    <div className="check"><Checkbox></Checkbox></div>
+                    <div className="check">
+                        {
+                            !item.needDownload ? 
+                            <Checkbox 
+                            onChange={(e) => props.changeDownloadVideos(e.target.checked, item.kbps)} 
+                            checked={props.downloadVideos.includes(item.kbps)}>
+                            </Checkbox> : 
+                            <span style={{fontSize: '12px', color: '#666'}}>下载中</span>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
@@ -58,8 +68,15 @@ function VideoLi (props) {
 @withRouter
 @connect(mapStateToProps)
 class CloudCreateContainer extends PureComponent {
+    state = {
+        downloadVideos: [],
+        ip: ''
+    }
     componentDidMount() {
         console.log(this.props)
+        this.setState({
+            ip: this.props.history.location.query.ip
+        });
         this.requestVideos();
     }
 
@@ -76,8 +93,31 @@ class CloudCreateContainer extends PureComponent {
 
     }
 
-    onChange () {
+    changeDownloadVideosAll = (e) => {
+        let downloadVideos = [];
+        if (e.target.checked) {
+            downloadVideos = this.props.currentDeviceVideos.filter(m => !m.needDownload).map(item => item.kbps);
+        }
+        this.setState({
+            downloadVideos
+        });
+    }
 
+    changeDownloadVideos = (checked, kbps) => {
+        let downloadVideos = this.state.downloadVideos;
+        if (checked) {
+            downloadVideos.push(kbps);
+        } else {
+            downloadVideos = downloadVideos.filter(m => m.kbps !== kbps);
+        }
+        this.setState({
+            downloadVideos
+        });
+    }
+
+    setDownload = () => {
+        ipcRenderer.send('change-device-videos-download', {videosKbps: this.state.downloadVideos, ip: this.state.ip});
+        this.toBack();
     }
 
     leftChildren () {
@@ -100,18 +140,27 @@ class CloudCreateContainer extends PureComponent {
         const {currentDeviceVideos} = this.props;
         return (
             <Fragment>
-                <PageHeader backTitle="设备视频详情" rightText="立即导入" leftChildren={this.leftChildren()} back={this.toBack}></PageHeader>
+                <PageHeader backTitle="设备视频详情" 
+                rightText="立即导入" 
+                rightClick={this.setDownload}
+                leftChildren={this.leftChildren()} 
+                back={this.toBack}></PageHeader>
                 <div className="page-container" style={{marginTop: 20}}>
                     <header className="video-import-header"> 
-                        <Checkbox onChange={this.onChange}>全选</Checkbox>
-                        <span>&nbsp;&nbsp;&nbsp;&nbsp;已选视频：18</span>
+                        <Checkbox onChange={this.changeDownloadVideosAll}>全选</Checkbox>
+                        <span>&nbsp;&nbsp;&nbsp;&nbsp;已选视频：{this.state.downloadVideos.length}</span>
                     </header>
                     <div className="videos-list">
                         <Row type="flex" style={{marginTop: 20}} gutter={[16,16]}>
                             {currentDeviceVideos.map(item => {
                                 return (
                                     <Col span={6}>
-                                    <VideoLi {...this.props} item={item}></VideoLi>
+                                    <VideoLi 
+                                    {...this.props} 
+                                    item={item} 
+                                    changeDownloadVideos={this.changeDownloadVideos} 
+                                    downloadVideos={this.state.downloadVideos}>
+                                    </VideoLi>
                                 </Col>
                                 )
                             })}
