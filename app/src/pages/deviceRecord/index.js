0c2bff331dbe12d3d89de2ec1a77610de1ed471a
pzl1026 <pzl1026@aliyun.com>
2020-01-23 11:06:59 +0800

    设备下载记录页面渲染

--- app/src/pages/deviceRecord/index.js
+++ app/src/pages/deviceRecord/index.js
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import {connect} from 'dva';
 import withRouter from 'umi/withRouter';
-import {Row, Col} from 'antd';
+import {Row, Col, Icon} from 'antd';
 import DownloadHeader from '@components/DownloadHeader';
 import {getLocalTime} from '@helper/utils';
 import './index.scss';
@@ -18,23 +18,23 @@ const headerFields = [
         type: 1,
         typeTitle: '正在下载',
         tableFields: [
-            '文件名称', '项目名称', '下载目录', '下载进度'
+            '设备名称', '导入视频数量', '导入目录', '导入进度'
         ],
-        colSpan: [6, 6, 6, 6]
+        colSpan: [7, 3, 7, 7]
     }, {
         type: 2,
         typeTitle: '下载成功',
         tableFields: [
-            '文件名称', '项目名称', '本地目录', '完成时间'
+            '设备名称', '导入视频数量', '导入目录', '完成时间'
         ],
-        colSpan: [6, 6, 6, 6]
+        colSpan: [7, 3, 7, 7]
     }, {
         type: 3,
         typeTitle: '下载失败',
         tableFields: [
-            '文件名称', '项目名称', '下载目录', '失败原因'
+            '设备名称', '导入视频数量', '导入目录', '失败原因'
         ],
-        colSpan: [6, 6, 6, 6]
+        colSpan: [7, 3, 7, 7]
     }
 ];
 
@@ -42,31 +42,34 @@ function DownlistLi(props) {
     console.log(props.videos, 'videosvideos')
     return props.videos.map(item => {
         let lastVal = '';
+        let count = 0;
         switch(props.type) {
             case 1:
-                let progress = parseInt(Number(item.downloadProgress) * 100);
-                lastVal = progress === 0 ? '等待中' : progress + '%';
+                lastVal = item.process;
+                count = item.downCount;
                 break;
             case 2:
                 lastVal = getLocalTime(item.successTime / 1000);
+                count = item.successCount;
                 break;
             case 3:
                 lastVal = item.failReason;
+                count = item.failCount;
                 break;
             default:
         }
         return (
             <Row className="dl-li" key={item.id}>
-                <Col span={6}>
-                    <span>{item.name}</span>
+                <Col span={7}>
+                    <span>{item.product.sn}</span>
                 </Col>
-                <Col span={6}>
-                    <span>{item.projectName}</span>
+                <Col span={3}>
+                    <span>{count}</span>
                 </Col>
-                <Col span={6}>
+                <Col span={7}>
                     <span>{item.localPath}</span>
                 </Col>
-                <Col span={6}>
+                <Col span={7}>
                     <span>{lastVal}</span>
                 </Col>
             </Row>
@@ -76,6 +79,8 @@ function DownlistLi(props) {
 
 function TabList(props) {
     const {typeActive} = props;
+    let videosRecords = props.videosRecords;
+    let isFail = videosRecords[2].videos.find(m => m.failCount > 0);
     return (
         <ul className="record-tabs">
             {headerFields.map(item => {
@@ -86,7 +91,7 @@ function TabList(props) {
                         : ''}
                         onClick={() => props.changeTab(item.type)}>
                         <span>{item.typeTitle}</span>
-                        <span></span>
+                        <sup style={{color: '#f00'}}>{item.type === 3 && isFail ? <Icon type="exclamation-circle" /> : null}</sup>
                     </li>
                 );
             })}
@@ -101,6 +106,7 @@ function TableList(props) {
         let currentVideo = props.videosRecords.find(m => m.type === item.type);
         if (!currentVideo) return null;
         // console.log(currentVideo.videos, 'videosss')
+     
         return (
             <div
                 key={item.type}
@@ -110,7 +116,7 @@ function TableList(props) {
                     ? 'none'
                     : ''
             }}>
-                <DownloadHeader header={item.tableFields} colSpan={item.colSpan}></DownloadHeader>
+                <DownloadHeader header={item.tableFields} colSpan={item.colSpan} videosRecords={props.videosRecords}></DownloadHeader>
                 <div className="table-list">
                     <DownlistLi videos={currentVideo.videos || []} type={item.type}/>
                 </div>
@@ -146,13 +152,14 @@ class CloudRecordContainer extends Component {
     componentDidMount() {
         console.log(this.props);
 
-        ipcRenderer.send('post-all-videos');
+        ipcRenderer.send('post-devices-videos');
         this.getVideoTimer = setInterval(() => {
             ipcRenderer.send('post-all-videos');
         }, 1000);
    
         // 获取所有的曾经下载过的跟即将下载以及失败的视频
-        ipcRenderer.on('get-all-videos', (event, allVideos) => {
+        ipcRenderer.on('get-devices-videos', (event, allVideos) => {
+            console.log(allVideos, 'allvides')
             this.getAllStatusVideos(allVideos);
         });
     }
@@ -164,9 +171,21 @@ class CloudRecordContainer extends Component {
     getAllStatusVideos(allVideos) {
         // console.log(allVideos, 'allVideos')
         let videosRecords = this.state.videosRecords;
-        videosRecords[0].videos = allVideos.filter(item => !item.isFail && !item.isSuccess);
-        videosRecords[1].videos = allVideos.filter(item => item.isSuccess);
-        videosRecords[2].videos = allVideos.filter(item => item.isFail);
+        videosRecords[0].videos = allVideos.map(item => {
+            let successCount = item['media-files'].filter(m => m.isSuccess).length;
+            item.downCount = item['media-files'].filter(m => m.needDownload && !m.isSuccess).length;
+            let count = item['media-files'].filter(m => m.needDownload).length;
+            item.process = (parseFloat(successCount / count).toFixed(2) * 100) + '%';
+            return item;
+        });
+        videosRecords[1].videos = allVideos.map(item => {
+            item.successCount = item['media-files'].filter(m => m.isSuccess).length;
+            return item;
+        });
+        videosRecords[2].videos = allVideos.map(item => {
+            item.failCount = item['media-files'].filter(m => m.isFail).length;
+            return item;
+        });
         this.setState({
             videosRecords
         });
@@ -185,7 +204,7 @@ class CloudRecordContainer extends Component {
 
         return (
             <Fragment>
-                <TabList {...this.props} typeActive={typeActive} changeTab={this.changeTab}/>
+                <TabList {...this.props} typeActive={typeActive} changeTab={this.changeTab} videosRecords={this.state.videosRecords}/>
                 <TableList {...this.props}  typeActive={typeActive} videosRecords={this.state.videosRecords}/> {/* <div className="download-list">
                     <DownloadHeader header={header}></DownloadHeader>
                     <DownlistLi></DownlistLi>
